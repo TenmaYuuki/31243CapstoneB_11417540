e6746b99206ba83b8e64df1e8544652f0be8f065
Nelson Q <kingdomheartsrule@hotmail.com>
2016-09-15 10:23:02 +1000

    Add files via upload

---
+++ scrapeEmojis.R
@@ -0,0 +1,84 @@
+# source: https://github.com/today-is-a-good-day/Emoticons/blob/master/scrapeEmoticons.R
+# libraries to load
+library(rvest)
+library(magrittr)
+library(dplyr)
+
+# reference website
+url <- "http://apps.timwhitlock.info/emoji/tables/unicode"
+
+# get emoticons
+emoticons <- url %>%
+  read_html() %>%
+  html_nodes(xpath='/html/body/div[2]/div/div/table[1]') %>%
+  html_table()
+emoticons <- data.frame(emoticons[[1]]$Native, emoticons[[1]]$Bytes, 
+                        emoticons[[1]]$Description, stringsAsFactors = FALSE)
+names(emoticons) <- c("Native", "Bytes", "Description")
+
+# get additional emoticons
+addemoticons <- url %>%
+  read_html() %>%
+  html_nodes(xpath='/html/body/div[2]/div/div/table[6]') %>%
+  html_table()
+addemoticons <- data.frame(addemoticons[[1]]$Native, addemoticons[[1]]$Bytes, 
+                           addemoticons[[1]]$Description, stringsAsFactors = FALSE)
+names(addemoticons) <- c("Native", "Bytes", "Description")
+
+# get dingbats
+dingbats <- url %>%
+  read_html() %>%
+  html_nodes(xpath='/html/body/div[2]/div/div/table[2]') %>%
+  html_table()
+dingbats <- data.frame(dingbats[[1]]$Native, dingbats[[1]]$Bytes, 
+                       dingbats[[1]]$Description, stringsAsFactors = FALSE)
+names(dingbats) <- c("Native", "Bytes", "Description")
+
+# get transports
+transport <- url %>%
+  read_html() %>%
+  html_nodes(xpath='/html/body/div[2]/div/div/table[3]') %>%
+  html_table()
+transport <- data.frame(transport[[1]]$Native, transport[[1]]$Bytes, 
+                        transport[[1]]$Description, stringsAsFactors = FALSE)
+names(transport) <- c("Native", "Bytes", "Description")
+
+# get additional transports
+addtransport <- url %>%
+  read_html() %>%
+  html_nodes(xpath='/html/body/div[2]/div/div/table[7]') %>%
+  html_table()
+addtransport <- data.frame(addtransport[[1]]$Native, addtransport[[1]]$Bytes, 
+                           addtransport[[1]]$Description, stringsAsFactors = FALSE)
+names(addtransport) <- c("Native", "Bytes", "Description")
+
+# get enclosed emoticons
+enclosed <- url %>%
+  read_html() %>%
+  html_nodes(xpath='/html/body/div[2]/div/div/table[4]') %>%
+  html_table()
+enclosed <- data.frame(enclosed[[1]]$Native, enclosed[[1]]$Bytes, 
+                       enclosed[[1]]$Description, stringsAsFactors = FALSE)
+names(enclosed) <- c("Native", "Bytes", "Description")
+
+# get uncategorised emoticons
+uncategorised <- url %>%
+  read_html() %>%
+  html_nodes(xpath='/html/body/div[2]/div/div/table[5]') %>%
+  html_table()
+uncategorised <- data.frame(uncategorised[[1]]$Native, uncategorised[[1]]$Bytes, 
+                            uncategorised[[1]]$Description, stringsAsFactors = FALSE)
+names(uncategorised) <- c("Native", "Bytes", "Description")
+
+# get additional other emoticons
+addothers <- url %>%
+  read_html() %>%
+  html_nodes(xpath='/html/body/div[2]/div/div/table[8]') %>%
+  html_table()
+addothers <- data.frame(addothers[[1]]$Native, addothers[[1]]$Bytes, 
+                        addothers[[1]]$Description, stringsAsFactors = FALSE)
+names(addothers) <- c("Native", "Bytes", "Description")
+
+# combine all dataframes to overall dataframe
+alltogether <- bind_rows(list(emoticons, addemoticons, dingbats, transport, 
+                              addtransport, enclosed, uncategorised, addothers))
