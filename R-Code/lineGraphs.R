189e2e88fa32c157cdd282840d65ccb64dc0a39b
Nelson Q <kingdomheartsrule@hotmail.com>
2016-10-05 16:37:57 +1100

    Update lineGraphs.R

--- R-Code/lineGraphs.R
+++ R-Code/lineGraphs.R
@@ -2,21 +2,21 @@
 # source: https://cosmosweb.champlain.edu/people/stevens/WebTech/R/Chapter-3-R.pdf
 
 # change directory if needed
-setwd("F:/University of Technology, Sydney/2016/Analytics Capstone Project B/R Code")
+setwd("xxxxxxxxxxxxxxxxxx")
 
-# Line graph for #PokémonGO
-PKMNGO_Dates <- read.table("Pokémon GO_Dates.csv",header=TRUE,sep=",")
+# Line graph for #PokÃ©monGO
+PKMNGO_Dates <- read.table("PokÃ©mon GO_Dates.csv",header=TRUE,sep=",")
 GO_date <- as.Date(PKMNGO_Dates$createdDate, "%d/%m/%Y")
 GO_frequency <- PKMNGO_Dates$frequency
 plot(GO_date,GO_frequency,type="o",xlab="Date",ylab="Frequency",
-     main="#PokémonGO - Tweets over time")
+     main="#PokÃ©monGO - Tweets over time")
 
-# Line graph for #PokémonSunMoon
-PKMNSM_Dates <- read.table("Pokémon Sun & Moon_Dates.csv",header=TRUE,sep=",")
+# Line graph for #PokÃ©monSunMoon
+PKMNSM_Dates <- read.table("PokÃ©mon Sun & Moon_Dates.csv",header=TRUE,sep=",")
 SM_date <- as.Date(PKMNSM_Dates$createdDate, "%d/%m/%Y")
 SM_frequency <- PKMNSM_Dates$frequency
 plot(SM_date,SM_frequency,type="o",xlab="Date",ylab="Frequency",
-     main="#PokémonSunMoon - Tweets over time")
+     main="#PokÃ©monSunMoon - Tweets over time")
 
 # Line graph for #YoKaiWatch
 YK_Dates <- read.table("YKWatch_Dates.csv",header=TRUE,sep=",")
